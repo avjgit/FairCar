a47c7682c1214c2d3aa07e9e80050f220efd23c5
AJ <avjmail@gmail.com>
2017-05-23 18:34:04 +0300

    Add project files.

---
+++ FairCar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
